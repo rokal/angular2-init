a03c8c7b02edaea052e0b5c71681648d02b18c2b
Roland <rkalmogo@modulusdata.com>
2016-10-26 15:43:06 -0400

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/browser-platform';
+
+import {AppComponent} from './app.component';
+
+@NgModule({
+    imports:[BrowserModule],
+    declarations:[AppComponent],
+    bootstrap:[AppComponent]
+})
+export class AppModule{}
